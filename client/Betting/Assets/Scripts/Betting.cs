0e19d9f88e7b88b8ba9029adb3cf5fa76106a5a4
Do Trung Kien <trungkien.keio@gmail.com>
2019-05-10 11:02:22 +0700

    update client

---
+++ client/Betting/Assets/Scripts/Betting.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Betting : MonoBehaviour
+{
+    public Text lastWinNumber;
+    public Text myAddress;
+
+    public Button[] numbers;
+
+    void Start()
+    {
+
+    }
+}
