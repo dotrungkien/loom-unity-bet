684815ded68aab91706d178805b36fd322de268a
Do Trung Kien <trungkien.keio@gmail.com>
2019-05-10 14:43:49 +0700

    connect with contract

--- client/Betting/Assets/Scripts/Betting.cs
+++ client/Betting/Assets/Scripts/Betting.cs
@@ -1,17 +1,106 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Loom.Unity3d;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Betting : MonoBehaviour
 {
+    public TextAsset bettingABI;
+    public TextAsset bettingAddress;
+
+
     public Text lastWinNumber;
     public Text myAddress;
+    public Text connectionStatus;
 
     public Button[] numbers;
 
-    void Start()
+    private byte[] privateKey;
+    private byte[] publicKey;
+    private Address from;
+    private EvmContract contract;
+
+    async void Start()
     {
+        Connect();
+        contract = await GetContract();
+        await StaticCallContract("totalSlots");
+    }
+
+    void Connect()
+    {
+        connectionStatus.text = "Connecting";
+        string privateKeyHex = PlayerPrefs.GetString("privateKeyHex", "");
+        if (privateKeyHex != "")
+        {
+            privateKey = CryptoUtils.HexStringToBytes(privateKeyHex);
+        }
+        else
+        {
+            privateKey = CryptoUtils.GeneratePrivateKey();
+            privateKeyHex = CryptoUtils.BytesToHexString(privateKey);
+            PlayerPrefs.SetString("privateKeyHex", privateKeyHex);
+        }
+
+        publicKey = CryptoUtils.PublicKeyFromPrivateKey(privateKey);
+        from = Address.FromPublicKey(publicKey);
+        myAddress.text = from.LocalAddressHexString;
+    }
+
+    async Task<EvmContract> GetContract()
+    {
+        var writer = RPCClientFactory.Configure()
+            .WithLogger(Debug.unityLogger)
+            .WithWebSocket("ws://127.0.0.1:46657/websocket")
+            .Create();
+
+        var reader = RPCClientFactory.Configure()
+            .WithLogger(Debug.unityLogger)
+            .WithWebSocket("ws://127.0.0.1:9999/queryws")
+            .Create();
+
+        var client = new DAppChainClient(writer, reader)
+        {
+            Logger = Debug.unityLogger
+        };
+
+        client.TxMiddleware = new TxMiddleware(new ITxMiddlewareHandler[]{
+            new NonceTxMiddleware{
+                PublicKey = publicKey,
+                Client = client
+            },
+            new SignedTxMiddleware(privateKey)
+        });
+
+        string abi = bettingABI.ToString();
+        var contractAddr = Address.FromHexString(bettingAddress.ToString());
+        return new EvmContract(client, contractAddr, from, abi);
 
     }
+
+    public async Task StaticCallContract(string func)
+    {
+        if (contract == null)
+        {
+            throw new Exception("Not signed in!");
+        }
+
+        Debug.Log("Calling smart contract...");
+
+        int result = await contract.StaticCallSimpleTypeOutputAsync<int>(func);
+        if (result != null)
+        {
+            Debug.Log("Smart contract returned: " + result);
+        }
+        else
+        {
+            Debug.LogError("Smart contract didn't return anything!");
+        }
+
+    }
+
 }
